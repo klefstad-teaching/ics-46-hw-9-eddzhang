7c31f8fb7d40f149fd84064ee9c36cf9f7473f27
eddzhang <ezhang345@gmail.com>
2025-03-15 00:15:17 +0000

    print_path removed error checking

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -52,12 +52,6 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 
 void print_path(const vector<int>& v, int total)
 {
-    if (v.empty())
-    {
-        cout << "no path" << endl;
-        return;
-    }
-
     for (int i = 0; i < v.size(); ++i)
     {
         cout << v[i] << " ";
